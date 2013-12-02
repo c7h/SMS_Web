3311ab494702f9f9a0ed714e1e57fd85a944aa14
c7h <christoph@gerneth.info>
2013-12-02 19:31:01 +0100

    include sipgate module

--- senden/sipgate_sms.py
+++ senden/sipgate_sms.py
@@ -5,13 +5,7 @@
 send SMS via Sipgate
 '''
 
-try:
-    import sipgate
-except ImportError:
-    print """
-    sipgate library required:
-    https://github.com/pklaus/python-sipgate-xmlrpc
-    """
+import sipgate
 import json
 from SMS_Web.settings import MY_SIPGATE_PASSWORD, MY_SIPGATE_USERNAME
     
@@ -21,14 +15,11 @@ s = sipgate.api(MY_SIPGATE_USERNAME, MY_SIPGATE_PASSWORD, 'UNICEF-Nikolausaktion
 default_uri = 'sip:NULL@sipgate.net'
 for own_uri in s.OwnUriListGet()['OwnUriList']:
     if own_uri['DefaultUri']: default_uri = own_uri['SipUri']
-        
-#debug:
-print "Default URI:", default_uri  
+ 
 
 def sendSMS(text, recipient_number):
     #clear number:
     recipient_number = recipient_number.replace("+", "").replace("-", "")
-    #remove newlines
     text = ' '.join(text.splitlines())
     print "sending SMS to", recipient_number, ":", text
     ## SessionInitiate may return the following server status codes in case of errors: 501, 502, 506, 520, 525
